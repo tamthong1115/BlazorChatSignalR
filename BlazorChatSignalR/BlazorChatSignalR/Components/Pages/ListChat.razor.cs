3dab1c008d9c701decadd7f55fd1923e84c55e65
Lenovo <caonhathao2372004@gmail.com>
2024-07-22 21:05:44 +0700

    create and edit layout. Add new listchat page

---
+++ BlazorChatSignalR/BlazorChatSignalR/Components/Pages/ListChat.razor.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorChatSignalR.Components.Pages
+{
+	public partial class ListChat
+	{
+		string nameAccount = "";
+		string lastTime = DateTime.Now.ToString("HH:mm");
+		string lastMessage = "";
+
+		public List<Chatroom> ChatList { get; set; } = new List<Chatroom>();
+	}
+
+	public class Chatroom
+	{
+		public string nameAccount { get; set; }
+		public string lastTime { get; set; }
+		public string lastMessage { get; set; }
+	}
+}
