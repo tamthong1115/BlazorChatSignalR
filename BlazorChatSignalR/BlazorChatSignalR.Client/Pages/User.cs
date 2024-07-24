ab7c3147940f7980751ab4b94f8c755659c4b2a5
Nguyen Dinh Hieu <nguyenhieu96h@gamil.com>
2024-07-24 20:52:11 +0700

    init project

---
+++ BlazorChatSignalR/BlazorChatSignalR.Client/Pages/User.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorChatSignalR.Client.Pages
+{
+    public class Chat
+    {
+        public int Id { get; set; }
+        public string Sender { get; set; }
+        public string Receiver { get; set; }
+        public string Message { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
